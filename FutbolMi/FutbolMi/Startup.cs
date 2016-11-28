cacc291ced97922096c2e6e3acc86a47aba13cec
nlthorne <nlthorne@wi.rr.com>
2016-11-28 17:23:05 -0600

    remaking repo

---
+++ FutbolMi/FutbolMi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FutbolMi.Startup))]
+namespace FutbolMi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
